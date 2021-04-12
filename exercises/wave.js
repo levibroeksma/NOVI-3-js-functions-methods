d3bff45cb57f83f0d094b770a09fe37276c355d1
Malou <maloua@users.noreply.github.com>
2021-04-12 11:56:14 +0200

    wave

---
+++ exercises/wave.js
@@ -0,0 +1,16 @@
+// Doe de wave!
+
+// Schrijf een functie die een string neemt als input
+// De functie geeft een array terug, waarbij één voor één elke letter een HOOFDLETTER is
+
+// Opdracht 1
+// wave("hello") // geeft ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+// Opdracht 2
+// Pas de functie zo aan, dat alle letters behalve de 'wave'-letter, klein zijn
+// wave("mSnTaaL") // geeft ['Msntaal','mSntaal','msNtaal','msnTaal','msntAal','msntaAl','msntaaL']
+
+// Opdracht 3
+// Pas de functie zo aan, dat andere symbolen de wave niet onderbreken
+// Dus geen 'hi-ya' in allemaal kleine letters, omdat het streepje (-) dat geen hoofdletter kan worden, wordt overgeslagen. Hint: 'a' !== 'A', maar '-' === '-'
+// wave("Hi-Ya") // geeft [ 'Hi-ya', 'hI-ya', 'hi-Ya', 'hi-yA']
