91d1829e10354ff1a57b00f165ed3b76499bacd3
School <School@Levis-oude-bakbeest.local>
2021-04-19 13:25:14 +0200

    solved assignment 3

--- exercises/wave.js
+++ exercises/wave.js
@@ -35,6 +35,7 @@ function stringWave1(string) {
 }
 
 console.log(stringWave1(wave1));
+console.log('\n')
 //
 // console.log('\n');
 // Opdracht 3
@@ -42,3 +43,18 @@ console.log(stringWave1(wave1));
 // Dus geen 'hi-ya' in allemaal kleine letters, omdat het streepje (-) dat geen hoofdletter kan worden, wordt overgeslagen. Hint: 'a' !== 'A', maar '-' === '-'
 // wave("Hi-Ya") // geeft [ 'Hi-ya', 'hI-ya', 'hi-Ya', 'hi-yA']
 
+const wave2 = 'Hi-ya';
+function stringWave2(string) {
+    for (let i = 0; i < string.length; i++) {
+        if(string[i] === '-') {
+            continue;
+        }
+        if(i === 0) {
+            console.log(string.charAt(i).toUpperCase() + string.slice(i+1).toLowerCase());
+        } else if(i > 0) {
+            console.log(string.slice(0, i).toLowerCase() + string.charAt(i).toUpperCase() + string.slice(i+1).toLowerCase());
+        }
+    }
+}
+
+console.log(stringWave2(wave2));
