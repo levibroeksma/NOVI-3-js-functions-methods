5928aaf34e6fb98036b7ae01337889daceebf4fb
School <School@Levis-oude-bakbeest.local>
2021-04-20 15:21:17 +0200

    fixed undifined issue based on feedback / tipsss

--- exercises/wave.js
+++ exercises/wave.js
@@ -7,36 +7,48 @@
 // wave("hello") // geeft ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
 
 const wave = 'hello';
+
 function stringWave(string) {
+    let waveArr = [];
     for (let i = 0; i < string.length; i++) {
+        let subString = '';
         if(i === 0) {
-            console.log(string.charAt(i).toUpperCase() + string.slice(i+1));
+            subString = string.charAt(i).toUpperCase() + string.slice(i+1);
         } else if(i > 0) {
-            console.log(string.slice(0, i) + string.charAt(i).toUpperCase() + string.slice(i+1));
+            subString = string.slice(0, i) + string.charAt(i).toUpperCase() + string.slice(i+1);
         }
+        waveArr.push(subString);
     }
+
+    return waveArr;
 }
 
 console.log(stringWave(wave));
+console.log('\n');
+
+
 
 // Opdracht 2
 // Pas de functie zo aan, dat alle letters behalve de 'wave'-letter, klein zijn
 // wave("mSnTaaL") // geeft ['Msntaal','mSntaal','msNtaal','msnTaal','msntAal','msntaAl','msntaaL']
 const wave1 = 'helLo';
 function stringWave1(string) {
+    let newArr = [];
     for (let i = 0; i < string.length; i++) {
+        let subString = '';
         if(i === 0) {
-            console.log(string.charAt(i).toUpperCase() + string.slice(i+1).toLowerCase());
+            subString = string.charAt(i).toUpperCase() + string.slice(i+1).toLowerCase();
         } else if(i > 0) {
-            console.log(string.slice(0, i).toLowerCase() + string.charAt(i).toUpperCase() + string.slice(i+1).toLowerCase());
+            subString = string.slice(0, i).toLowerCase() + string.charAt(i).toUpperCase() + string.slice(i+1).toLowerCase();
         }
-
+        newArr.push(subString)
     }
+    return newArr;
 }
 
 console.log(stringWave1(wave1));
 console.log('\n')
-//
+
 // console.log('\n');
 // Opdracht 3
 // Pas de functie zo aan, dat andere symbolen de wave niet onderbreken
@@ -45,16 +57,20 @@ console.log('\n')
 
 const wave2 = 'Hi-ya';
 function stringWave2(string) {
+    let againNewArr = [];
     for (let i = 0; i < string.length; i++) {
+        let subString = '';
         if(string[i] === '-') {
             continue;
         }
         if(i === 0) {
-            console.log(string.charAt(i).toUpperCase() + string.slice(i+1).toLowerCase());
+            subString = string.charAt(i).toUpperCase() + string.slice(i+1).toLowerCase();
         } else if(i > 0) {
-            console.log(string.slice(0, i).toLowerCase() + string.charAt(i).toUpperCase() + string.slice(i+1).toLowerCase());
+            subString = string.slice(0, i).toLowerCase() + string.charAt(i).toUpperCase() + string.slice(i+1).toLowerCase();
         }
+        againNewArr.push(subString);
     }
+    return againNewArr;
 }
 
-console.log(stringWave2(wave2));
+console.log(stringWave2(wave2))
